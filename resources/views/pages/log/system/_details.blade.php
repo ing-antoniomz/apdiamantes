7a8a1cc9080f7fc16fa6e178ac51249bdf20fb9e
amzuniga <amzuniga@finanzas.cdmx.gob.mx>
2025-04-17 17:59:42 -0600

    ajuste visualizacion logs

--- resources/views/pages/log/system/_details.blade.php
+++ resources/views/pages/log/system/_details.blade.php
@@ -1,3 +1,3 @@
-<textarea readonly cols="130" rows="10">
-    {!! json_encode($content, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) !!}
-</textarea>
+<pre style="max-height: 500px; background-color: white; color: black; padding: 1em; border-radius: 5px;">
+<code style="background-color: #aea7a7cd; color: black;">{!! json_encode($content, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) !!}</code>
+</pre>
